403d7b5435a8392833c36b7a62d140abd02dfeba
Popa Vlad <popa.vlad94@gmail.com>
2024-04-20 01:04:56 +0300

    parse csv data

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-import assert from 'assert';
-import { parse } from 'csv-parse';
+const fs = require('fs');
+const Papa = require('papaparse');
 
-const records = [];
-// Initialize the parser
-const parser = parse({
-  delimiter: ','
+const csvFile = fs.readFileSync('./data/urls.csv', 'utf8');
+
+const parsedData = Papa.parse(csvFile, {
+  header: true,
+  dynamicTyping: true,
 });
-// Use the readable stream api to consume records
-parser.on('readable', function(){
-  let record;
-  while ((record = parser.read()) !== null) {
-    records.push(record);
-  }
-});
-// Catch any error
-parser.on('error', function(err){
-  console.error(err.message);
-});
-// Test that the parsed records matched the expected records
-parser.on('end', function(){
-  assert.deepStrictEqual(
-    records,
-    [
-      [ 'root','x','0','0','root','/root','/bin/bash' ],
-      [ 'someone','x','1022','1022','','/home/someone','/bin/bash' ]
-    ]
-  );
-});
-// Write data to the stream
-parser.write("root:x:0:0:root:/root:/bin/bash\n");
-parser.write("someone:x:1022:1022::/home/someone:/bin/bash\n");
-// Close the readable stream
-parser.end();
+
+console.log(parsedData.data);
