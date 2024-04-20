bd4bf29e2e2a36d20f699d7f48c6a4c3e955310b
Popa Vlad <popa.vlad94@gmail.com>
2024-04-20 13:26:05 +0300

    add categories, more products and running time

--- helpers/retrieveProducts.js
+++ helpers/retrieveProducts.js
@@ -1,18 +1,26 @@
 function getProductUrls(data) {
     const productUrls = {};
+    const categoryUrls = {};
     let count = 0;
+    let countCategories = 0;
   
     for (const domain in data) {
       productUrls[domain] = [];
+      categoryUrls[domain] = [];
   
       for (const url of data[domain]) {
-        if (url.includes("/product/") || url.includes("?product=")) {
+        if (url.includes("/product/") || url.includes("?product=") || url.includes("product")) {
           productUrls[domain].push(url);
           count++;
+
+          if (url.includes("/category/") || url.includes("?category=") || url.includes("category")) {
+            countCategories++;
+          }
         }
       }
     }
-    console.log(count);
+    console.log("No of products: ", count);
+    console.log("No of categories: ", countCategories);
     return productUrls;
   }
 
--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const Papa = require('papaparse');
 
 const groupByDomain = require('./helpers/groupByDomain');
 const retrieveProducts = require('./helpers/retrieveProducts');
+const { time } = require('console');
 
 const csvFile = fs.readFileSync('./data/urls.csv', 'utf8');
+
+let timeBefore = Date.now();
 
 const parsedData = Papa.parse(csvFile, {
   header: true,
@@ -18,9 +21,16 @@ for (let i = 0; i < parsedData.data.length; i++) {
 
 groupByDomain(urls)
   .then(groupedDomains => {
-    console.log("Domains grouped successfully:", groupedDomains);
-    retrieveProducts.getProductUrls(groupByDomain);
+    // console.log("Domains grouped successfully:", groupedDomains);
+    let products = retrieveProducts.getProductUrls(groupedDomains);
+    console.log(products);
   })
   .catch(error => {
     console.error("Error grouping domains:", error);
   });
+
+let timeAfter = Date.now();
+
+console.log("Running time: ", timeAfter - timeBefore);
+
+// console.log(parsedData.data);
